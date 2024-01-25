e7e73f48b8a04805292fc27c68b918cfb73d6d1e
Rahul Narayanan <rahulon@icloud.com>
2024-01-25 09:57:39 -0500

    Initial Commit

---
+++ MotionCards/MotionCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionCardsApp.swift
+//  MotionCards
+//
+//  Created by Rahul on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MotionCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
